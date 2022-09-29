f3d6cc7572397cec9c705a418da97003ff015466
prj <errgrr@gmail.com>
2022-09-29 18:24:16 +0300

    Code modifications for DigiSpark

--- HumidifierSensor.ino
+++ HumidifierSensor.ino
@@ -1,13 +1,14 @@
+#include <SendOnlySoftwareSerial.h>
 #include <CapacitiveSensor.h>
 
-#define SENDER_PIN 3
+#define SENDER_PIN 0
 #define SENSOR_PIN 2
 
 #define SAMPLES_NUMBER 1000
 #define TIMEOUT_MS 500
 
-#define MIN_READING 4200 // for 1M resistor and 1000 samples
-#define MAX_READING 11000 // for 1M resistor and 1000 samples
+#define MIN_READING 2850 // for 1M resistor and 1000 samples with P2 as sensor pin
+#define MAX_READING 9200 // for 1M resistor and 1000 samples with P2 as sensor pin
 
 byte packet[] = {0xFA, 0x29, 0x03, 0x00, 0x00, 0x00, 0x00, 0x14, 0x9A, 0x00, 0x00, 0x00, 0x03, 0x77, 0x72, 0x71, 0x03, 0x00, 0x6C, 0x4C, 0x3B, 0x03, 0x2F, 0x15, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x2c, 0x02, 0x6D, 0x37, 0xD2, 0x00};
 
@@ -15,13 +16,14 @@ byte checksum;
 byte mappedValue;
 long readingRaw;
 
-CapacitiveSensor sensor = CapacitiveSensor(SENDER_PIN,SENSOR_PIN); // 1M resistor between pins 3 & 2, pin 2 is sensor pin
+CapacitiveSensor sensor = CapacitiveSensor(SENDER_PIN,SENSOR_PIN); // 1M resistor between pins 0 & 2, pin 2 is sensor pin
+SendOnlySoftwareSerial outSerial(1); // Software TX serial on pin 1, the LED is also on this pin. No problem for serial, but not good for capacitive sensor.
 
 void setup()                    
 {
    sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate
    sensor.set_CS_Timeout_Millis(TIMEOUT_MS);
-   Serial.begin(9600);
+   outSerial.begin(9600);
 }
 
 void loop()                    
@@ -46,8 +48,11 @@ void loop()
     
   packet[42] = checksum; //last byte checksum
   
-  Serial.write(packet, sizeof(packet));
-  //Serial.println(readingRaw); //for calibration
+  // Real output:
+  outSerial.write(packet, sizeof(packet));
+
+  // For calibration/debugging:
+  //outSerial.println(readingRaw);
 
   delay(100);
 }
